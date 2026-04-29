ee2302c61c0a7c392edae1ac02be8f42ade202c9
ep5377123-sys <ep5377123@gmail.com>
2026-04-29 11:59:49 -0400

    Charles Jumping Game

---
+++ #Charles Jumping Game.py
@@ -0,0 +1,102 @@
+#Charles Jumping Game
+
+#Source: 
+
+
+import pygame
+from pygame.locals import *
+import os
+import random
+
+pygame.init()
+
+W, H = 800, 437
+win = pygame.display.set_mode((W,H))
+pygame.display.set_caption('Side Scroller')
+
+bg = pygame.image.load(os.path.join('images', 'bg.png')).convert()
+bgX = 0
+bgX2 = bg.get_width()
+
+clock = pygame.time.Clock()
+
+class player(object):
+    run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8, 16)]
+    jump = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(1, 8)]
+    #slide = [pygame.image.load(os.path.join('images', 'S1.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S3.png')), pygame.image.load(os.path.join('images', 'S4.png')), pygame.image.load(os.path.join('images', 'S5.png'))]
+    fall = pygame.image.load(os.path.join('images', '0.png'))
+    jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.jumping = False
+        self.sliding = False
+        self.falling = False
+        self.slideCount = 0
+        self.jumpCount = 0
+        self.runCount = 0
+        self.slideUp = False
+
+    def draw(self, win):
+        if self.falling:
+            win.blit(self.fall, (self.x, self.y + 30))
+        elif self.jumping:
+            self.y -= self.jumpList[self.jumpCount] * 1.3
+            win.blit(self.jump[self.jumpCount//18], (self.x, self.y))
+            self.jumpCount += 1
+            if self.jumpCount > 108:
+                self.jumpCount = 0
+                self.jumping = False
+                self.runCount = 0
+            self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+        elif self.sliding or self.slideUp:
+            if self.slideCount < 20:
+                self.y += 1
+                self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+            elif self.slideCount == 80:
+                self.y -= 19
+                self.sliding = False
+                self.slideUp = True
+            elif self.slideCount > 20 and self.slideCount < 80:
+                self.hitbox = (self.x, self.y+3, self.width-8, self.height-35)
+
+            if self.slideCount >= 110:
+                self.slideCount = 0
+                self.runCount = 0
+                self.slideUp = False
+                self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+            win.blit(self.slide[self.slideCount//10], (self.x, self.y))
+            self.slideCount += 1
+
+        else:
+            if self.runCount > 42:
+                self.runCount = 0
+            win.blit(self.run[self.runCount//6], (self.x,self.y))
+            self.runCount += 1
+            self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-13)
+
+        #pygame.draw.rect(win, (255,0,0),self.hitbox, 2)
+def redrawGameWindow(): #draws background and objects (all drawings happen in one place)
+    win.blit(bg, (bgX,0))
+    win.blit(bg, (bgX2,0))
+    pygame.display.update()
+speed = 30 
+clock.tick(speed) #gets the FPS
+run = True
+while run: #while game is running
+    redrawGameWindow()
+    clock.tick(speed) #sets the FPS
+    bgX -= 1.4
+    bgX2 -= 1.4
+    if bgX < bg.get_width() * -1: #first background image starts at 0 , 0 then start maoving backwwards then gets to the negative width, so it is off the screen
+        bgX = bg.get_width()
+    if bgX2 < bg.get_width() * -1:
+        bgX2 = bg.get_width()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+
