d25e6ed810d54a3d0c4685a1b541040291a11b33
charlescampbell732 <charlescampbell732@gmail.com>
2026-05-03 17:21:13 -0400

    Update flingtest.py

--- flingtest.py
+++ flingtest.py
@@ -1,9 +1,15 @@
 import pygame
+import random
+import sys
+import numpy as np
+from bomb_configs import *
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
+vent = pygame.image.load("img_keys/vent.png").convert_alpha()
 rect_surf = pygame.Surface((300, 232), pygame.SRCALPHA) 
 rect_surf.fill((255, 255, 255))
 
@@ -21,24 +27,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                fling = True
+        if RPi:
+            pressed = component_keypad.pressed_keys
+        else:
+            pressed = []
 
     screen.fill((30, 30, 30))
 
-    rotated_surf = pygame.transform.rotate(rect_surf, angle)
-    
+    if len(pressed) > 3:
+        pressed = []
+
+    print(len(pressed))
+        
+    if pressed == [4, 1, 2, 3]:
+        fling = True
+        
     if fling:
         count += 1
         angle += 20
         rx -= 40
         ry -= 40
+    
+    fvent = pygame.transform.rotate(vent, angle)
 
     if fling:
-        screen.blit(rotated_surf, (rx,ry))
+        screen.blit(fvent, (rx,ry))
     else:
-        screen.blit(rect_surf, (rx, ry))
+        screen.blit(fvent, (rx, ry))
     
     pygame.display.flip()
     clock.tick(60)
